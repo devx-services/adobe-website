f362e97dfa2f2f998a12fe861835670786d3a8a0
Iryna Lagno <ilagno@adobe.com>
2023-02-14 10:09:18 -0600

    - initial commit for cards component

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,24 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
+
   /* change to ul, li */
-  const ul = document.createElement('ul');
+
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+    row.className = 'cards-item';
+    [...row.children].forEach((div) => {
+      if (div.innerHTML === '') {
+        div.remove();
+      }
+      if (div.querySelector('a') && div.childElementCount === 1) {
+        div.className = 'card-link';
+      } else if (div.querySelector('picture')) {
+        div.className = 'card-image'
+      } else {
+        div.className = 'card-text'
+      };
+        block.querySelectorAll('.primary').forEach(((link) => link.classList.remove("button", "primary")))
+        block.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])))
     });
-    ul.append(li);
-  });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
+  })
 }
