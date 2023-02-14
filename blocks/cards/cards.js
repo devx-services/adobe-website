84df77dcf1c7f9205d90726ce300e388554decf8
Iryna Lagno <ilagno@adobe.com>
2023-02-14 14:37:56 -0600

    - add additional styles

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,24 +1,37 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-
 export default function decorate(block) {
 
   /* change to ul, li */
 
   [...block.children].forEach((row) => {
     row.className = 'cards-item';
+    let rowImageCount = 0;
     [...row.children].forEach((div) => {
+      //remove empty divs
       if (div.innerHTML === '') {
         div.remove();
       }
       if (div.querySelector('a') && div.childElementCount === 1) {
         div.className = 'card-link';
-      } else if (div.querySelector('picture')) {
+      } else if (div.querySelector('picture') && rowImageCount < 1) {
+        //style for background images
         div.className = 'card-image'
+        rowImageCount++;
+
+      } else if (div.querySelector('picture') && rowImageCount >= 1) {
+        //style for images to overlap
+        div.className = 'card-image-centered'
       } else {
         div.className = 'card-text'
-      };
-        block.querySelectorAll('.primary').forEach(((link) => link.classList.remove("button", "primary")))
-        block.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])))
+        // add horizontal line
+        if (div.querySelectorAll('h2').length > 1) {
+          //add vertical line after heading
+          div.querySelectorAll('h2').forEach(((heading) => {
+            let hrContainer = document.createElement('hr');
+            heading.after(hrContainer)
+          }));
+        }
+      }
+      block.querySelectorAll('.primary').forEach(((link) => link.classList.remove("button", "primary")))
     });
   })
 }
