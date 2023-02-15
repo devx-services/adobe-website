353783855956d0f4c29cbcf7face7f3411f59c5d
Olena Orobei <oorobei@adobe.com>
2023-02-15 17:09:56 -0600

    Fix styles

--- blocks/panel/panel.js
+++ blocks/panel/panel.js
@@ -1,11 +1,11 @@
 export default function init(el) {
-    const children = el.querySelectorAll(':scope > div');
-    const foreground = children[children.length - 1];
-    if (children.length > 1) {
-        children[0].classList.add('background');
-    }
-    foreground.classList.add('foreground', 'container');
-    const headline = foreground.querySelector('h1, h2, h3, h4, h5, h6');
-    const text = headline.closest('div');
-    text.classList.add('text');
+  const children = el.querySelectorAll(':scope > div');
+  const foreground = children[children.length - 1];
+  if (children.length > 1) {
+    children[0].classList.add('background');
+  }
+  foreground.classList.add('foreground', 'container');
+  const headline = foreground.querySelector('h1, h2, h3, h4, h5, h6');
+  const text = headline.closest('div');
+  text.classList.add('text');
 }
