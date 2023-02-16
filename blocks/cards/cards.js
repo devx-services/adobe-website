9fcb2f049116064519ac8758ac5920571e35f936
Iryna Lagno <ilagno@adobe.com>
2023-02-16 15:14:07 -0600

    - add additional styles

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,37 +1,50 @@
 export default function decorate(block) {
+  /**
+   *
+   * @param url
+   * @returns {string}
+   */
+  function getBackgroundUrl(url) {
+    return `url('${url}')`;
+  }
 
-  /* change to ul, li */
+  /**
+   *
+   * @param row
+   * @param imageProvider
+   */
+  function setBackgroundOnRow(row, imageProvider) {
+    if (imageProvider.querySelector('picture > img')) {
+      row.style.backgroundImage = getBackgroundUrl(imageProvider.querySelector('picture > img').src);
+    } else {
+      row.style.backgroundColor = imageProvider.textContent;
+    }
+    row.removeChild(imageProvider);
+  }
 
   [...block.children].forEach((row) => {
-    row.className = 'cards-item';
-    let rowImageCount = 0;
+    row.classList.add('cards-item');
+    /* set background image */
+    setBackgroundOnRow(row, row.children[0]);
     [...row.children].forEach((div) => {
-      //remove empty divs
+      /* remove empty divs */
       if (div.innerHTML === '') {
         div.remove();
       }
-      if (div.querySelector('a') && div.childElementCount === 1) {
-        div.className = 'card-link';
-      } else if (div.querySelector('picture') && rowImageCount < 1) {
-        //style for background images
-        div.className = 'card-image'
-        rowImageCount++;
-
-      } else if (div.querySelector('picture') && rowImageCount >= 1) {
-        //style for images to overlap
-        div.className = 'card-image-positioned'
+      if (div.querySelector('picture')) {
+        /* style for images to overlap */
+        div.classList.add('card-image');
+      } else if (div.querySelectorAll('h2') && div.querySelectorAll('.button-container').length > 1) {
+        /* add vertical line after heading */
+        div.querySelectorAll('h2').forEach(((heading) => {
+          const hrContainer = document.createElement('hr');
+          heading.after(hrContainer);
+          div.classList.add('card-list');
+        }));
       } else {
-        div.className = 'card-text'
-        // add horizontal line
-        if (div.querySelectorAll('h2') && div.querySelectorAll('.button-container').length > 1) {
-          //add vertical line after heading
-          div.querySelectorAll('h2').forEach(((heading) => {
-            let hrContainer = document.createElement('hr');
-            heading.after(hrContainer)
-          }));
-        }
+        div.classList.add('card-text');
       }
-      block.querySelectorAll('.primary').forEach(((link) => link.classList.remove("button", "primary")))
+      block.querySelectorAll('.primary').forEach(((link) => link.classList.remove('button', 'primary')));
     });
-  })
+  });
 }
