999366348930666bc8bae331d80c032d4a9a40f2
Amy Zhao <azhao@adobe.com>
2023-02-17 10:46:12 -0600

    Footer (#10)
    * add styling
    
    ---------
    
    Co-authored-by: Amy Zhao <azhao@adobe.net>

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,4 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
@@ -6,14 +6,38 @@ import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
  */
 
 export default async function decorate(block) {
+  /*
+  const ul = document.createElement('div');
+  ul.className = 'footer-menu';
+  [...block.children].forEach((row) => {
+    //const li = document.createElement('div');
+    //li.innerHTML = row.innerHTML;
+    let contents = row.innerHTML;
+    ul.innerHTML += contents;
+  });
+  block.textContent = '';
+  block.append(ul);
+  */
   const cfg = readBlockConfig(block);
   block.textContent = '';
-
   const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`);
   const html = await resp.text();
   const footer = document.createElement('div');
+  footer.className = 'footer-wrap';
   footer.innerHTML = html;
-  await decorateIcons(footer);
+  const footerBlock = footer.querySelector(':scope > div');
+  if (footerBlock.firstElementChild) {
+    const menu = footerBlock.firstElementChild;
+    menu.className = 'footer-menu';
+  }
+  if (footerBlock.nextElementSibling) {
+    const featuredProducts = footerBlock.nextElementSibling;
+    featuredProducts.className = 'footer-featured-products';
+    if (featuredProducts.nextElementSibling) {
+      const copyright = featuredProducts.nextElementSibling;
+      copyright.className = 'copyright';
+    }
+  }
   block.append(footer);
 }
